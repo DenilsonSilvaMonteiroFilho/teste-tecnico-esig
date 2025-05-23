8acc321219855f643615f8f9c923229c46427a80
Denilson <de_nilson_filho@hotmail.com>
2025-05-23 09:46:47 -0300

    Projeto inicial com estrutura Spring Boot + JSF + Maven

---
+++ src/main/java/com/denilson/teste_tecnico_esig/TesteTecnicoEsigApplication.java
@@ -0,0 +1,13 @@
+package com.denilson.teste_tecnico_esig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTecnicoEsigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTecnicoEsigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denilson/teste_tecnico_esig/TesteTecnicoEsigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denilson.teste_tecnico_esig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTecnicoEsigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
